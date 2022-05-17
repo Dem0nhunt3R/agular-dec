d2e229268dc6b205aa39c053300b5d69aa66746d
d3m0nhunt3r <d3m0nhunt3r92@gmail.com>
2022-05-17 21:58:05 +0300

    lesson 1 hw

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {IPost} from "../interfaces";
+import {urls} from "../constants";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getPosts():Observable<IPost[]>{
+    return this.httpClient.get<IPost[]>(urls.posts);
+  }
+
+  getPostById(id:number):Observable<IPost>{
+    return this.httpClient.get<IPost>(urls.posts+'/'+id);
+  }
+}
