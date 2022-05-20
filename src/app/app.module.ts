f9a012b9627e76b867bf3c6f75a3610ed8132b3b
d3m0nhunt3r <d3m0nhunt3r92@gmail.com>
2022-05-21 00:56:08 +0300

    lesson 4 hw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {HeaderComponent} from './components';
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/user/models/user-todos.interface.ts
@@ -0,0 +1,6 @@
+export interface IUserTodos {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+}
